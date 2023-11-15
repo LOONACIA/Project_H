eff4aa6b5fe212326ebdb4224ee420b8b219b38e
bagzaru <bagzaru3690@gmail.com>
2023-11-15 14:18:31 +0900

    [Feat]: 공격 가능 기간 여유롭게 변경

--- Assets/Scripts/Weapons/MeleeWeapon.cs
+++ Assets/Scripts/Weapons/MeleeWeapon.cs
@@ -21,15 +21,14 @@ public class MeleeWeapon : Weapon
 
     //콤보 공격 관련 변수
     private bool m_canNextAttack = false;
-    
+
     protected override void Attack()
     {
         //TODO: 이번 공격의 End보다 다음 공격의 Start가 먼저 호출될 수 있음.
         //공격 중인데 다음 공격 딜레이가 오지 않았다면 return;
         //if (IsAttacking && !m_canNextAttack) return;
-        
-        if(State is AttackState.HIT or AttackState.FOLLOW_THROUGH or AttackState.IDLE)
-            Animator.SetTrigger(MonsterAttack.s_attackAnimationKey);
+
+        Animator.SetTrigger(MonsterAttack.s_attackAnimationKey);
     }
 
     #region AnimationEvent
@@ -41,26 +40,26 @@ public class MeleeWeapon : Weapon
     protected override void OnHitMotion()
     {
         //TODO: 1인칭일 경우 카메라 쉐이킹
-        
+
         //공격 관련 변수 초기화
         IsAttacking = true;
         //m_isHitBoxChecked = false;
         m_hitEventIndex = 0;
-        
+
         //TODO: 무기별로 콤보 대기 타이밍이 다름. 현재는 애니메이션 기준이지만, 후에 시간 기준으로 변경할지 논의 필요.
         m_canNextAttack = false;
-        
+
         attackedEnemyList.Clear();
         Animator.SetBool(MonsterAttack.s_targetCheckAnimationKey, false);
     }
-    
+
     protected override void OnFollowThroughMotion()
     {
         IsAttacking = false;
     }
 
     #endregion
-    
+
     #region UnityEvent
 
     private void Update()
@@ -69,11 +68,11 @@ public class MeleeWeapon : Weapon
         {
             //MeleeWeapon은 hit 판정 프레임 중 한번이라도 적에게 닿았다면 바로 취소합니다.
             //if (m_isHitBoxChecked) return;
-        
+
             //내 몬스터와 다른 대상만 가져옴
-            var detectedObjects 
+            var detectedObjects
                 = attackHitBox.DetectHitBox(transform)
-                              .Where(hit=>hit.gameObject!=Owner.gameObject);
+                              .Where(hit => hit.gameObject != Owner.gameObject);
 
             foreach (var detected in detectedObjects)
             {
@@ -98,7 +97,7 @@ public class MeleeWeapon : Weapon
     }
 
     #endregion
-    
+
     #region HitBox
 
     private void OnDrawGizmosSelected()
@@ -107,4 +106,5 @@ public class MeleeWeapon : Weapon
     }
 
     #endregion
+
 }
