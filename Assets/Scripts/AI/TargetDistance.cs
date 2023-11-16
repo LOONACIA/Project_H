464164a7b19f716fec62a979e76790a2d925bb07
acessiane <70795118+acessiane@users.noreply.github.com>
2023-11-16 18:06:31 +0900

    Fix: BT 무한 Idle 수정
    Abort 조건 잘 확인할것

--- Assets/Scripts/AI/TargetDistance.cs
+++ Assets/Scripts/AI/TargetDistance.cs
@@ -9,6 +9,7 @@ namespace BehaviorDesigner.Runtime.Tasks.Unity.Math
     {
         [SerializeField]
         private SharedTransform m_target;
+        public SharedTransform m_self;
         private Transform m_position;
         public float m_distance;
 
@@ -17,13 +18,15 @@ namespace BehaviorDesigner.Runtime.Tasks.Unity.Math
         // Start is called before the first frame update
         public override TaskStatus OnUpdate()
         {
+            m_targetDistance = Vector3.Distance(m_target.Value.position, m_self.Value.position);
+            Debug.Log(m_targetDistance);
             //return bool1.Value == bool2.Value ? TaskStatus.Success : TaskStatus.Failure;
             return m_targetDistance <= m_distance ? TaskStatus.Success : TaskStatus.Failure;
         }
 
         public override void OnReset()
         {
-            m_targetDistance = Vector3.Distance(m_target.Value.position, this.transform.position);
+            
         }
     }
 }
