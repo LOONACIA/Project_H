f81bbb2aa303b6c19a34bedb4e2dd5b2fd412cda
MinseokLee <leems041625@gmail.com>
2023-11-23 13:28:06 +0900

    [Merge]

--- Assets/Scripts/Effects/CameraEffect.cs
+++ Assets/Scripts/Effects/CameraEffect.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CyberCameraEffect : MonoBehaviour
+public class CameraEffect : MonoBehaviour
 {
     #region PublicVariables
     #endregion
@@ -14,6 +14,16 @@ public class CyberCameraEffect : MonoBehaviour
     public void OnCombo1Ready()
     {
         GameManager.Camera.Animator.SetTrigger(ConstVariables.CAMERA_ANIMATORPARAMETER_COMBO1READY);
+    }
+
+    public void OnCombo1Play()
+    {
+        GameManager.Camera.Animator.SetTrigger(ConstVariables.CAMERA_ANIMATORPARAMETER_COMBO1PLAY);
+    }
+
+    public void OnCombo1Recovery()
+    {
+        GameManager.Camera.Animator.SetTrigger(ConstVariables.CAMERA_ANIMATORPARAMETER_COMBO1RECOVERY);
     }
     #endregion
 
