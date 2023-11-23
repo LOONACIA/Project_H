095b99ec8f2c98cfba90475a6afe55d603646a28
acessiane <70795118+acessiane@users.noreply.github.com>
2023-11-23 14:29:26 +0900

    Feat: WaveManager BT 어그로 수정

--- Assets/Scripts/Trigger/FirstZoneTrigger.cs
+++ Assets/Scripts/Trigger/FirstZoneTrigger.cs
@@ -1,23 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using UnityEngine;
 
-public class FirstZoneTrigger : MonoBehaviour
+public class WaveTrigger : MonoBehaviour
 {
+    public readonly ObservableCollection<Monster> Monsters = new();
     [SerializeField]
-    private GameObject m_firstZone;
+    private int m_leftMonster;
+    [SerializeField]
+    private GameObject[] m_gameObjects;
 
-    private void OnTriggerEnter(Collider other)
+    private void Awake()
     {
-        var character = FindObjectsOfType<Monster>()
-            .SingleOrDefault(monster => monster.IsPossessed);
+        Monsters.CollectionChanged += OnMonsterCollectionChanged;
+    }
 
-        if(other.name == character.name)
+    private void OnMonsterCollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+    {
+        if (e.OldItems != null)
         {
-            Debug.Log(other);
-            m_firstZone.SetActive(true);
-            Destroy(this);
+            foreach (var monster in e.OldItems.OfType<Monster>())
+            {
+                monster.Dying -= OnMonsterDying;
+            }
         }
+
+        if (e.NewItems != null)
+        {
+            foreach (var monster in e.NewItems.OfType<Monster>())
+            {
+                monster.Dying += OnMonsterDying;
+            }
+        }
+    }
+
+    private void OnMonsterDying(object sender, System.EventArgs e)
+    {
+        // 스폰된 몬스터들 전부 죽음(플레이어 빼고)
+        var count = Monsters.Count(monster => !monster.IsPossessed);
+        if (count <= m_leftMonster)
+        {
+            for(int i = 0; i < m_gameObjects.Length; i++)
+            {
+                m_gameObjects[i].SetActive(false);
+            }
+        }
+
+        if (sender is not Monster monster)
+        {
+            return;
+        }
+
+        Monsters.Remove(monster);
     }
 }
