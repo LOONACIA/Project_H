ed0c1f7aa50a2ca665baa6341d95f4556b01bde9
acessiane <70795118+acessiane@users.noreply.github.com>
2023-12-02 15:52:58 +0900

    Fix: BT Patrol

---
+++ Assets/Scripts/AI/ChangeLeader.cs
@@ -0,0 +1,10 @@
+using BehaviorDesigner.Runtime.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLeader : Action
+{
+    // Start is called before the first frame update
+
+}
