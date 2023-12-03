d9b723c3775828191d54b98ea765126471a8d3d1
LOONACIA <91052167+LOONACIA@users.noreply.github.com>
2023-12-03 14:58:11 +0900

    [Fix] 세이브 포인트 오류 수정

--- Assets/Scripts/InteractableObjects/GateMachine.cs
+++ Assets/Scripts/InteractableObjects/GateMachine.cs
@@ -21,13 +21,6 @@ public class GateMachine : InteractableObject
         m_gateScript = m_gate.GetComponent<IGate>();
     }
 
-    public override void Interact(Actor actor, IProgress<float> progress, Action onComplete)
-    {
-        base.Interact(actor, progress, onComplete);
-
-        Alarm();
-    }
-
     protected override void OnInteract(Actor actor)
     {
         IsInteractable = false;
@@ -35,42 +28,6 @@ public class GateMachine : InteractableObject
         if (m_gate != null && m_gateScript != null)
         {
             StartCoroutine(m_gateScript.Open());
-        }
-    }
-
-    /// <summary>
-    /// 몬스터들에게 플레이어를 공격하라고 알림
-    /// </summary>
-    private void Alarm()
-    {
-        FindMonsters();
-
-        var character = m_monsters.SingleOrDefault(monster => monster.IsPossessed);
-
-        foreach (var monster in m_monsters)
-        {
-            if (monster == character)
-                continue;
-
-            monster.Targets.Add(character);
-        }
-    }
-
-    /// <summary>
-    /// 범위 내에 있는 몬스터를 리스트에 저장
-    /// </summary>
-    private void FindMonsters()
-    {
-        var monsterColliders = Physics.OverlapSphere(transform.position, m_alarmRadius, LayerMask.GetMask("Monster"));
-
-        m_monsters.Clear();
-
-        foreach (var monsterCollider in monsterColliders) 
-        {
-            if (monsterCollider.gameObject.TryGetComponent<Monster>(out var monster))
-            {
-                m_monsters.Add(monster);
-            }
         }
     }
 
