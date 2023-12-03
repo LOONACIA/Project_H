798c098ea65cb1f169da10f077b2a09b709304ec
bagzaru <bagzaru3690@gmail.com>
2023-12-03 15:25:36 +0900

    [Feat]: 미사용 줄 삭제

--- Assets/Scripts/Trigger/DeadZone.cs
+++ Assets/Scripts/Trigger/DeadZone.cs
@@ -11,11 +11,7 @@ public class DeadZone : MonoBehaviour
         //TODO: hit이 가능한 Layer만 가져오기 (최적화)
         if (other.TryGetComponent<Actor>(out var actor))
         {
-            DamageInfo info = new DamageInfo(99999, actor.transform.position - transform.position, actor.transform.position, actor);
-            //actor.Health.TakeDamage(info);
             actor.Health.Kill();
         }
-        
-        
     }
 }
