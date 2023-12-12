d8ced058d8d36c72b558e79bb67469c121cd3d22
정경세 <gyeongse2797@gmail.com>
2023-12-12 20:09:36 +0900

    [Feat] : HackingLaser 추가
    HackingLaser 프리팹, 스크립트 추가

---
+++ Assets/Scripts/Objects/Laser.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VolumetricLines;
+
+[RequireComponent(typeof(CapsuleCollider))]
+[RequireComponent(typeof(VolumetricLineBehavior))]
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float m_progressTime = 0.5f;
+
+    private CapsuleCollider m_collider;
+
+    private VolumetricLineBehavior m_volumetricLineBehavior;
+
+    private float m_originWidth;
+
+    private void Start()
+    {
+        m_collider = GetComponent<CapsuleCollider>();
+        m_volumetricLineBehavior = GetComponent<VolumetricLineBehavior>();
+
+        m_collider.center = (m_volumetricLineBehavior.StartPos + m_volumetricLineBehavior.EndPos) / 2;
+        m_collider.height = MathF.Abs(m_volumetricLineBehavior.StartPos.y - m_volumetricLineBehavior.EndPos.y);
+
+        m_originWidth = m_volumetricLineBehavior.LineWidth;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Monster"))
+        {
+            if (collision.gameObject.TryGetComponent<ActorHealth>(out var health))
+            {
+                // TODO : 데미지 계산
+            }
+        }
+    }
+
+    public void Hacking()
+    {
+        m_collider.isTrigger = true;
+
+        StartCoroutine(IE_ChangeWidth(m_originWidth, 0));
+    }
+
+    public void Recovery()
+    {
+        m_collider.isTrigger = false;
+
+        StartCoroutine(IE_ChangeWidth(0, m_originWidth));
+    }
+
+    private IEnumerator IE_ChangeWidth(float from, float to)
+    {
+        float time = 0;
+
+        while (time < m_progressTime)
+        {
+            time += Time.deltaTime;
+
+            m_volumetricLineBehavior.LineWidth = Mathf.Lerp(from, to, time / m_progressTime);
+
+            yield return null;
+        }
+    }
+}
