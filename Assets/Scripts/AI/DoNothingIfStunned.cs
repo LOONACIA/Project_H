be87c38be39bbcc4828e5144c47a52c7380a2973
LOONACIA <91052167+LOONACIA@users.noreply.github.com>
2023-12-13 14:15:54 +0900

    [Fix] AI 스턴 시 미끄러지는 문제 수정

--- Assets/Scripts/AI/DoNothingIfStunned.cs
+++ Assets/Scripts/AI/DoNothingIfStunned.cs
@@ -1,18 +1,32 @@
 using BehaviorDesigner.Runtime.Tasks;
+using UnityEngine.AI;
 
 public class DoNothingIfStunned : Action
 {
     private ActorStatus m_status;
+
+    private NavMeshAgent m_navMeshAgent;
     
     public override void OnAwake()
     {
         base.OnAwake();
 
         m_status = GetComponent<ActorStatus>();
+        m_navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    public override void OnStart()
+    {
+        m_navMeshAgent.isStopped = m_status.IsStunned;
     }
 
     public override TaskStatus OnUpdate()
     {
         return m_status.IsStunned ? TaskStatus.Running : TaskStatus.Failure;
+    }
+
+    public override void OnEnd()
+    {
+        m_navMeshAgent.isStopped = false;
     }
 }
