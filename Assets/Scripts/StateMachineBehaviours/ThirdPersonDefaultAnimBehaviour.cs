2836a956b0149b2fb90a242e03e479539bff0b49
bagzaru <bagzaru3690@gmail.com>
2023-12-18 23:02:49 +0900

    [Feat]: TP Animator들에 IsHitPlaying 변수 추가

--- Assets/Scripts/StateMachineBehaviours/ThirdPersonDefaultAnimBehaviour.cs
+++ Assets/Scripts/StateMachineBehaviours/ThirdPersonDefaultAnimBehaviour.cs
@@ -5,11 +5,13 @@ public class ThirdPersonDefaultAnimBehaviour : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.ResetTrigger("Attack");
-        animator.ResetTrigger("Hit");
-        animator.ResetTrigger("Dead");
+        animator.ResetTrigger(ConstVariables.ANIMATOR_PARAMETER_ATTACK);
+        animator.ResetTrigger(ConstVariables.ANIMATOR_PARAMETER_HIT);
+        animator.ResetTrigger(ConstVariables.ANIMATOR_PARAMETER_DEAD);
         //animator.ResetTrigger("KnockDown");
         animator.ResetTrigger("KnockBack");
+
+        animator.SetBool(ConstVariables.ANIMATOR_PARAMETER_IS_HIT_PLAYING, false);
         
         //공격 무기에 IDLE 시그널 보냄
         AttackAnimationEventReceiver receiver = animator.GetComponent<AttackAnimationEventReceiver>();
