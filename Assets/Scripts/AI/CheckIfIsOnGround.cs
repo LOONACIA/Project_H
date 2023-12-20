96ae996631ec58ab00bd5d0ff16783bf364564c9
acessiane <70795118+acessiane@users.noreply.github.com>
2023-12-20 17:14:16 +0900

    Feat: Monster AI & Shooter Holster

--- Assets/Scripts/AI/CheckIfIsOnGround.cs
+++ Assets/Scripts/AI/CheckIfIsOnGround.cs
@@ -2,20 +2,24 @@ using BehaviorDesigner.Runtime.Tasks;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CheckIfIsOnGround : Conditional
 {
     private Monster m_monster;
+
+    private NavMeshAgent m_navMeshAgent;
 
     public override void OnAwake()
     {
         base.OnAwake();
 
         m_monster = GetComponent<Monster>();
+        m_navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     public override TaskStatus OnUpdate()
     {
-        return m_monster.Movement.IsOnGround ? TaskStatus.Success : TaskStatus.Failure;
+        return m_navMeshAgent.enabled || m_monster.Movement.IsOnGround ? TaskStatus.Success : TaskStatus.Failure;
     }
 }
