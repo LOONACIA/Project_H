a1ece946e9867dbb0d03b770d70f1c2793fc7b74
LOONACIA <91052167+LOONACIA@users.noreply.github.com>
2023-12-23 12:23:06 +0900

    [Feat] Scene 관리 스크립트

---
+++ Assets/Scripts/Scenes/BossStage.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossStage : MonoBehaviour
+{
+	[SerializeField]
+	private AudioClip m_bgm;
+
+	private void Awake()
+	{
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	private void Start()
+	{
+		if (m_bgm != null)
+		{
+			GameManager.Sound.Play(m_bgm, SoundType.Bgm);
+		}
+	}
+}
