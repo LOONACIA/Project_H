37de3d18475a31e3a2f5b431a7b07733ad0a74f6
acessiane <70795118+acessiane@users.noreply.github.com>
2023-12-24 16:59:49 +0900

    Fix: UiDialogue

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -66,7 +66,7 @@ public partial class PlayerController : MonoBehaviour
         
         //GameManager.UI.ShowShurikenIndicator(m_possession);
         GameManager.UI.ShowDamageIndicator();
-        GameManager.UI.ShowObjects();
+        //GameManager.UI.ShowObjects();
         //GameManager.UI.ShowSkillIndicator(this);
     }
 
