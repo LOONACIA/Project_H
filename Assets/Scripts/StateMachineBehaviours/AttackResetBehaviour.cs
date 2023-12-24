e20fd4a4d61e02dec0872614d45fa0749aca685b
bagzaru <bagzaru3690@gmail.com>
2023-12-24 21:16:18 +0900

    [Feat]: AI가 공격 중 넉백에 맞았을 때 공격 상태가 유지되던 문제 해결, 넉백 모션 중 뛰어오던 오류 해결

---
+++ Assets/Scripts/StateMachineBehaviours/AttackResetBehaviour.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// 3인칭 넉백, 스턴 등 공격 State를 초기화해주는 이벤트가 필요할 때 사용됩니다.
+/// </summary>
+public class AttackResetBehaviour : StateMachineBehaviour
+{
+    private IEventProxy m_eventProxy;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+        if (m_eventProxy == null)
+        {
+            m_eventProxy = animator.GetComponent<IEventProxy>();
+        }
+
+        //공격 무기에 IDLE 시그널 보냄
+        m_eventProxy.DispatchEvent($"On{nameof(WeaponState.Idle)}");
+    }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (m_eventProxy == null)
+        {
+            m_eventProxy = animator.GetComponent<IEventProxy>();
+        }
+
+        //공격 무기에 IDLE 시그널 보냄
+        m_eventProxy.DispatchEvent($"On{nameof(WeaponState.Idle)}");
+    }
+}
