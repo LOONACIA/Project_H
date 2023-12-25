6089e04b8578ae7574c70b8a4bb3d056787fe4cb
bagzaru <bagzaru3690@gmail.com>
2023-12-25 18:46:37 +0900

    [Feat]: 샷건 이펙트 nullref 오류 수정

--- Assets/Scripts/Effects/BulletParticleEffect.cs
+++ Assets/Scripts/Effects/BulletParticleEffect.cs
@@ -20,8 +20,21 @@ public class BulletParticleEffect : MonoBehaviour
 
     private void Init()
     {
+
+        if(m_oldParent != null)
+        {
+            //이전에 초기화된 적이 있음. return;
+            return;
+        }
+
+        if(transform.parent == null)
+        {
+            Debug.LogError("샷건 이펙트의 부모 총알이 존재하지 않음.");
+        }
+
         vfx = GetComponent<VisualEffect>();
         //부모가 사라져도 일정시간 Trail을 남기기 위해, 부모를 분리합니다.
+
         m_oldParent = transform.parent;
         transform.parent = null;
 
@@ -36,10 +49,18 @@ public class BulletParticleEffect : MonoBehaviour
         {
             return;
         }
+        if(m_oldParent == null)
+        {
+            //부모가 없으면 이펙트 발동 x
+            return;
+        }
 
         if (!m_oldParent)
         {
+            //부모가 Destroy되었다면, 1초 뒤 삭제하는 절차를 밟음
             CoroutineEx.Create(this, IE_End());
+            m_oldParent = null;
+            return;
         }
 
 
@@ -57,9 +78,9 @@ public class BulletParticleEffect : MonoBehaviour
         {
             if (m_visualized)
             {
+                m_visualized = false;
                 vfx.SendEvent("OnEnd");
                 vfx.SetBool("TrailAlive", false);
-                m_visualized = false;
             }
         }
     }
