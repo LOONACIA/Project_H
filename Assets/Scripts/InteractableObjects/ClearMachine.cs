bdf6ee58825e395f283ca495912bec0f9e1ca5e1
bagzaru <bagzaru3690@gmail.com>
2024-01-12 16:35:38 +0900

    [Feat]: 엔딩 크레딧 변경

--- Assets/Scripts/InteractableObjects/ClearMachine.cs
+++ Assets/Scripts/InteractableObjects/ClearMachine.cs
@@ -1,3 +1,7 @@
+using LOONACIA.Unity.Managers;
+using System.Collections;
+using UnityEngine;
+
 public class ClearMachine : InteractableObject
 {
     protected override void OnInteract(Actor actor)
@@ -5,6 +9,11 @@ public class ClearMachine : InteractableObject
         if (actor.IsPossessed)
         {
             GameManager.Instance.SetGameClear();
+
+            //24.01.12: 클리어 시 EndingScene으로 넘어가는 것으로 변경.
+            IsInteractable = false;
+            ManagerRoot.Input.Disable<CharacterInputActions>();
+            _ = SceneHelper.LazyLoadAsync(nameof(SceneName.EndingScene));
         }
     }
 }
